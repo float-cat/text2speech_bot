7d0ca23396e3ed18b77f38d28d304407e5630bea
Float Cat <float.cat@localhost.localdomain>
2021-02-01 23:13:39 +0700

    pre-commit

--- t2s/speechkit.py
+++ t2s/speechkit.py
@@ -1,35 +1,40 @@
 import requests
 
+
 def checklanguage(text):
-    rus = 0;
-    eng = 0;
+    rus = 0
+    eng = 0
     for char in text.lower():
-        if char >= 'а' and char <= 'я':
+        if char >= "а" and char <= "я":
             rus += 1
-        elif char >= 'a' and char <= 'z':
+        elif char >= "a" and char <= "z":
             eng += 1
     if eng < rus:
-        return 'ru-RU'
-    return 'en-US'
+        return "ru-RU"
+    return "en-US"
+
 
 def synthesize(folder_id, iam_token, text, bot_config):
-    url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
+    url = "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize"
     headers = {
-        'Authorization': 'Bearer ' + iam_token,
+        "Authorization": "Bearer " + iam_token,
     }
 
     data = {
-        'text': text,
-        'lang': bot_config['lang'],
-        'folderId': folder_id
-        'speed': 1,
-        'voice': bot_config['voice'],
-        'emotion': 'good'
+        "text": text,
+        "lang": bot_config["lang"],
+        "folderId": folder_id,
+        "speed": 1,
+        "voice": bot_config["voice"],
+        "emotion": "good",
     }
 
     with requests.post(url, headers=headers, data=data, stream=True) as resp:
         if resp.status_code != 200:
-            raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
+            raise RuntimeError(
+                "Invalid response received: code: %d, message: %s" %
+                (resp.status_code, resp.text)
+            )
 
         for chunk in resp.iter_content(chunk_size=None):
             yield chunk
