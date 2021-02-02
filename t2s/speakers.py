ba43c9235e641071523bc4930feaefa170e67655
vtrokhymenko <8581044+vtrokhymenko@users.noreply.github.com>
2021-02-02 19:41:59 +0200

    E0xextazy create mvp bot (#16)
    * add .py file
    
    * add test file
    
    * delete test file
    
    * mvp-bot
    
    * Добавлено меню, выбор спикера, смена пола, смена языка на лету
    
    * pre-commit
    
    * Обновление ЧМВ интерфейса бота (#14)
    
    * Бот с минимальным функционалом
    
    * Исправлена комманда /changesex
    
    * trasfer code @cat to folder
    
    * transfer code @cat
    
    * Добавлено меню, выбор спикера, смена пола, смена языка на лету
    
    * pre-commit
    
    Co-authored-by: Float Cat <float.cat@localhost.localdomain>
    Co-authored-by: vtrokhymenko <8581044+vtrokhymenko@users.noreply.github.com>
    
    * Коррекция выбора спикеров, чтобы избежать падения из-за языка
    
    * Change start speaker nickname
    
    * Доработка выбора спикера и добавление диалога выбора скорости
    
    * Доработка выбора спикера
    
    * Доработка выбора спикера
    
    * Переработка кода в ООП
    
    * Pre-Pull-Request
    
    * Исправление ошибки в функции synthesize
    
    * Исправление ошибки в функции synthesize
    
    * Коррекция
    
    * Добавлены голоса Филипп и Алена, изменен выбор скорости
    
    * Переход на API_KEY
    
    * Pre-Commit
    
    * Revert "Pre-Commit"
    
    This reverts commit 8235b41e6c5918fbc794257c9ca03eb2384514ca.
    
    * Pre-Commit
    
    Co-authored-by: e0xextazy <e0xextazy@gmail.com>
    Co-authored-by: Float Cat <float.cat@localhost.localdomain>
    Co-authored-by: float-cat <78111252+float-cat@users.noreply.github.com>

---
+++ t2s/speakers.py
@@ -0,0 +1,24 @@
+# Speackers names
+class Speakers(object):
+    def __init__(self):
+        self.__speakers = {}
+        self.__speakers["female"] = {
+            "Алена": "alena",
+            "Элис": "alyss",
+            "Джейн": "jane",
+            "Оксана": "oksana",
+            "Омаж": "omazh",
+        }
+        self.__speakers["male"] = {"Филипп": "filipp", "Захар": "zahar", "Ермил": "ermil"}
+
+    def getSpeakersBySex(self, sex):
+        speakerlist = ""
+        for speaker in self.__speakers[sex].keys():
+            speakerlist += speaker + " "
+        return speakerlist
+
+    def isSpeaker(self, name, sex):
+        return name in self.__speakers[sex].keys()
+
+    def getSpeakerId(self, name, sex):
+        return self.__speakers[sex][name]
