5b3e3e3917cdc6e7bcd4b6633392fb9ebe4ce0e0
vtrokhymenko <8581044+vtrokhymenko@users.noreply.github.com>
2021-02-02 19:41:59 +0200

    E0xextazy create mvp bot (#16)
    * add .py file
    
    * add test file
    
    * delete test file
    
    * mvp-bot
    
    * Добавлено меню, выбор спикера, смена пола, смена языка на лету
    
    * pre-commit
    
    * Обновление ЧМВ интерфейса бота (#14)
    
    * Бот с минимальным функционалом
    
    * Исправлена комманда /changesex
    
    * trasfer code @cat to folder
    
    * transfer code @cat
    
    * Добавлено меню, выбор спикера, смена пола, смена языка на лету
    
    * pre-commit
    
    Co-authored-by: Float Cat <float.cat@localhost.localdomain>
    Co-authored-by: vtrokhymenko <8581044+vtrokhymenko@users.noreply.github.com>
    
    * Коррекция выбора спикеров, чтобы избежать падения из-за языка
    
    * Change start speaker nickname
    
    * Доработка выбора спикера и добавление диалога выбора скорости
    
    * Доработка выбора спикера
    
    * Доработка выбора спикера
    
    * Переработка кода в ООП
    
    * Pre-Pull-Request
    
    * Исправление ошибки в функции synthesize
    
    * Исправление ошибки в функции synthesize
    
    * Коррекция
    
    * Добавлены голоса Филипп и Алена, изменен выбор скорости
    
    * Переход на API_KEY
    
    * Pre-Commit
    
    * Revert "Pre-Commit"
    
    This reverts commit 8235b41e6c5918fbc794257c9ca03eb2384514ca.
    
    * Pre-Commit
    
    Co-authored-by: e0xextazy <e0xextazy@gmail.com>
    Co-authored-by: Float Cat <float.cat@localhost.localdomain>
    Co-authored-by: float-cat <78111252+float-cat@users.noreply.github.com>

---
+++ t2s/speechkit.py
@@ -0,0 +1,38 @@
+import requests
+
+
+def checklanguage(text):
+    rus = 0
+    eng = 0
+    for char in text.lower():
+        if char >= "а" and char <= "я":
+            rus += 1
+        elif char >= "a" and char <= "z":
+            eng += 1
+    if eng < rus:
+        return "ru-RU"
+    return "en-US"
+
+
+def synthesize(folder_id, api_key, text, userinfo):
+    url = "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize"
+    headers = {"Authorization": "Api-Key " + api_key}
+    voiceid = userinfo.getVoice()
+    if voiceid == "filipp":
+        voiceid = "zahar"
+    elif voiceid == "alena":
+        voiceid = "alyss"
+    data = {
+        "text": text,
+        "lang": userinfo.getLang(),
+        "folderId": folder_id,
+        "speed": userinfo.getSpeed(),
+        "voice": voiceid,
+    }
+
+    with requests.post(url, headers=headers, data=data, stream=True) as resp:
+        if resp.status_code != 200:
+            raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
+
+        for chunk in resp.iter_content(chunk_size=None):
+            yield chunk
