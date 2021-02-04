3531a5f53a9ef60bf085fd74a9cb397a09848d58
e0xextazy <49094658+e0xextazy@users.noreply.github.com>
2021-02-04 23:53:30 +0700

    Update preprocessing.py

--- t2s/preprocessing.py
+++ t2s/preprocessing.py
@@ -2,9 +2,9 @@ import re
 
 
 def TextPreprocessing(text):
-    text = re.sub(r"<script>[\w\W]*?<\/script>", "", text)
-    text = re.sub(r"<button>[\w\W]*?<\/button>", "", text)
-    text = re.sub(r"(\<(/?[^>]+)>)", " ", text)
+    text = re.sub(r"<script[\w\W]*?<\/script>", "", text)
+    text = re.sub(r"<button[\w\W]*?<\/button>", "", text)
+    text = re.sub(r"(\<(\/?[^>]+)>)", " ", text)
     text = re.sub(r"http[\w\W]*? ", " Здесь ссылка ", text)
     text = re.sub(r"\s+", " ", text)
     return text
