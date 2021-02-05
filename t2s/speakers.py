7a49d6844a5f4b6a427a3a1b4b65d528f7e6fef0
vtrokhymenko <8581044+vtrokhymenko@users.noreply.github.com>
2021-02-05 16:56:35 +0000

    refactor: add some new lines

--- t2s/speakers.py
+++ t2s/speakers.py
@@ -2,7 +2,9 @@
 
 
 def checklanguage(text):
+  
     for char in text.lower():
+  
         if char >= "а" and char <= "я":
             return "ru-RU"
         else:
@@ -10,10 +12,13 @@ def checklanguage(text):
 
 
 class Speakers(object):
+ 
     def __init__(self):
+ 
         self.__speakers = {}
         self.__speakers["ru-RU"] = {"male": "filipp", "female": "alena"}
         self.__speakers["en-US"] = {"male": "nick", "female": "alyss"}
 
     def getSpeakerId(self, lang, sex):
+ 
         return self.__speakers[lang][sex]
