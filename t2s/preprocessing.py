f202449e8e3bf8a935ab9f0dc51b4a0d9d09d7ba
vtrokhymenko <8581044+vtrokhymenko@users.noreply.github.com>
2021-02-05 15:42:37 +0000

    style: applied

--- t2s/preprocessing.py
+++ t2s/preprocessing.py
@@ -5,13 +5,13 @@ def TextPreprocessing(text):
 
     text = re.sub(r"<script[\w\W]*?<\/script>", "", text)
     text = re.sub(r"<button[\w\W]*?<\/button>", "", text)
-    
+
     # delete html tags
     text = re.sub(r"(\<(\/?[^>]+)>)", " ", text)
-    
+
     # delete links
     text = re.sub(r"http\S+", " ссылка ", text)
-    
+
     # removing extra spaces
     text = re.sub(r"\s+", " ", text)
 
